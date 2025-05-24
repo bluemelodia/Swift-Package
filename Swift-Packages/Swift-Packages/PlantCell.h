13a590b0bfff63347b9a3fe6daa112b150a4dcbd
Melanie H <mlh2197@columbia.edu>
2025-05-23 22:39:47 -0400

    chore: add Objective C subclass of CommonAncestor.

---
+++ Swift-Packages/Swift-Packages/PlantCell.h
@@ -0,0 +1,14 @@
+//
+//  PlantCell.h
+//  Swift-Packages
+//
+//  Created by Guac on 5/23/25.
+//
+
+#import "CommonAncestor.h";
+
+@interface PlantCell: CommonAncestor
+
+- (void)photosynthesize;
+
+@end
---
+++ Swift-Packages/Swift-Packages/PlantCell.m
@@ -0,0 +1,17 @@
+//
+//  PlantCell.m
+//  Swift-Packages
+//
+//  Created by Guac on 5/23/25.
+//
+
+#import <Foundation/Foundation.h>
+#import "PlantCell.h"
+
+@implementation PlantCell
+
+- (void) photosynthesize {
+    NSLog(@"Basking in the sun is so fun.");
+}
+
+@end
