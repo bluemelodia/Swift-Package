72f944a187e101e9f2c20d3928d9687b92fda827
Melanie H <mlh2197@columbia.edu>
2025-05-23 21:50:17 -0400

    chore: add starter project.

---
+++ Swift-Packages/Swift-Packages/Swift_PackagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_PackagesApp.swift
+//  Swift-Packages
+//
+//  Created by Guac on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Swift_PackagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
