4fd0d08afb13f82f346f84174ce4e4b7e465f1e4
Melanie H <mlh2197@columbia.edu>
2025-05-25 19:51:12 -0400

    chore: built AlienSymbiote as an .xcframework.

---
+++ output/AlienSymbiote.xcarchive/Products/Library/Frameworks/AlienSymbiote.framework/Headers/AlienSymbiote.h
@@ -0,0 +1,21 @@
+//
+//  AlienSymbiote.h
+//  AlienSymbiote
+//
+//  Created by Guac on 5/25/25.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef AlienSymbiote_h
+#define AlienSymbiote_h
+
+@interface AlienSymbiote : NSObject
+
+- (void) introduce;
+
+@end
+
+#endif /* AlienSymbiote_h */
+
+#import <Foundation/Foundation.h>
